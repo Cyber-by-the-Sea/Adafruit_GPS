605305d5c43cb874c570b349ef8c5046542885be
devtty1er <devtty1er@gmail.com>
2022-07-22 03:57:02 +0000

    wtf

---
+++ harness.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+
+#include <Adafruit_GPS.h>
+
+Adafruit_GPS GPS;
+
+#define FUZZER_MAX_BUFFER_LEN MINMEA_MAX_SENTENCE_LENGTH + 3 + 1
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    char* sentence = (char*) calloc(size + 3, sizeof(uint8_t));
+    memcpy(sentence, data, size);
+    GPS.addChecksum(sentence);
+    //printf("%s\n", sentence);
+    GPS.parse(sentence); // this also sets the newNMEAreceived() flag to false
+    return 0;
+}
