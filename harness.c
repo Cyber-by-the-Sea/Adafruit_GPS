b6d558b68770183eb51bc6999c498a17b51c089d
devtty1er <devtty1er@gmail.com>
2022-07-21 23:14:15 -0400

    Oof

--- harness.c
+++ harness.c
@@ -3,5 +3,13 @@
 Adafruit_GPS GPS;
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        return;
+    }
+    int len = strlen(argv[1]);
+    char* sentence = calloc(len + 3);
+    memcpy(sentence, argv[1], len);
+    GPS.addChecksum(sentence);
+    printf(sentence);
     GPS.parse(argv[1]); // this also sets the newNMEAreceived() flag to false
 }
